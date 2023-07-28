d6e68c2a09b62a10a9bf81a3ac2bfb82ea946a95
Aadhib <aadhibnasserv.hussain@gmail.com>
2023-07-29 00:34:37 +0530

    Add files via upload

---
+++ src/data/socials.ts
@@ -0,0 +1,19 @@
+export const socials = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://instagram.com/artyycraft',
+  },
+  
+  {
+    title: 'Facebook',
+    icon: 'mdi:facebook',
+    url: 'https://facebook.com/artyz',  
+  },
+
+  {
+    title: 'YouTube',
+    icon: 'mdi:youtube',
+    url: 'https://youtube.com/artyz',
+  }
+]
